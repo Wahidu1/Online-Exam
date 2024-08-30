f5d4caf2fc3a68d873c1922d9c4ff3c21a2ee9ab
Wahidul Islam <wahidulislambd4@gmail.com>
2024-08-30 21:30:34 +0600

    Something Change

--- onlineExam/settings.py
+++ onlineExam/settings.py
@@ -124,8 +124,8 @@ MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "wahidulislamofficialbd@gmail.com"
-EMAIL_HOST_PASSWORD = "udgi dpyv bbqv jbvb"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
